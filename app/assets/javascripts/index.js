94a26de03f06dfea5de43a19260c8d986334906a
Przemek <przemek@krowinski.eu>
2015-01-06 17:59:45 +0100

    Add style changes to the menu, calendar and buttons.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $(".note .btn-edit").click(function(){
     $("#"+$(this).data("id")+" .hidden-form").slideToggle();
   });
+
+  $(".link-block").click(function(){
+    var addr = $(this).find("a").attr("href");
+    window.location.href = addr;
+  });
 });
