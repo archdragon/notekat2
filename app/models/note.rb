bb04fa94b8667d6d064049244add92ca3b80c4d8
Przemek <przemek@krowinski.eu>
2015-01-06 03:14:39 +0100

    Add validations for notebook name.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,6 @@
 class Note < ActiveRecord::Base
   belongs_to :notebook
+
   has_paper_trail :meta => { :notebook_name => :notebook_name }
   acts_as_taggable
 
--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,4 +1,5 @@
 class Notebook < ActiveRecord::Base
   belongs_to :user
   has_many :notes
+  validates :name, presence: true
 end
