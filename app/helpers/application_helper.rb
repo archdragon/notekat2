23b1418820fd1d56baf81f3a0f8f57d733696679
Przemek <przemek@krowinski.eu>
2015-01-07 21:47:48 +0100

    Add devise to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,18 @@ module ApplicationHelper
       controller_name
     end
   end
+
+  # For Devise
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
