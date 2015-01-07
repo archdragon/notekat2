5b10aee0d53bcfde31a820ebb5773b6e921eb812
Przemek <przemek@krowinski.eu>
2015-01-07 04:03:59 +0100

    Add deployment settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'notekat'
+set :repo_url, 'git@github.com:archdragon/notekat2.git'
+
+set :deploy_to, "/home/deployer/apps/notekat"
+
+# Default value for :scm is :git
+set :scm, :git
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
