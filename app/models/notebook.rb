1c12cad4957f319a8f5952e91286de59d844de20
Przemek <przemek@krowinski.eu>
2015-01-10 01:48:19 +0100

    Add name length verification

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,5 +1,5 @@
 class Notebook < ActiveRecord::Base
   belongs_to :user
   has_many :notes
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 1, maximum: 150 }
 end
