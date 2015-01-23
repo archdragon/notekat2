23665039ef288b8c5b13115d82e9ebaded01b2c1
Przemek <przemek@krowinski.eu>
2015-01-23 15:27:58 +0100

    Fix typos

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 describe Note do
+  let(:note) { FactoryGirl.build(:note) }
   describe "#notebook_name" do
+    let(:notebook) { FactoryGirl.build(:notebook) }
     it "gets the name of the parent notebook" do
-
+      expect(note.notebook_name).to be_equal(notebook.name)
     end
   end
 end
