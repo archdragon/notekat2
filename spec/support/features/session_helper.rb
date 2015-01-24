d43f30b83afea925b240fc7d8f84d6d4ab821a71
Przemek <przemek@krowinski.eu>
2015-01-24 19:29:58 +0100

    Add session helper

---
+++ spec/support/features/session_helper.rb
@@ -0,0 +1,27 @@
+module Features
+  module SessionHelpers
+    def sign_up_with(email, password)
+      visit sign_up_path
+      fill_in 'Email', with: email
+      fill_in 'Password', with: password
+      click_button 'Sign up'
+    end
+
+    def sign_in_with(email, password)
+      #visit new_user_session_path
+      visit "/"
+      click_link "Log in"
+      fill_in 'Email', with: email
+      fill_in 'Password', with: password
+      click_button 'Log in'
+    end
+
+    def sign_in
+      user = create(:user)
+      visit sign_in_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+    end
+  end
+end
