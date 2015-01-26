49c718863b0c0f0ebd29755fed97a80e223d44c6
Przemek <przemek@krowinski.eu>
2015-01-26 05:41:17 +0100

    Fix login path

--- spec/support/features/session_helper.rb
+++ spec/support/features/session_helper.rb
@@ -1,28 +1,25 @@
 module Features
   module SessionHelpers
     def sign_up_with(email, password)
-      visit sign_up_path
+      visit new_user_registration_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
+      fill_in 'Password confirmation', with: password
       click_button 'Sign up'
     end
 
     def sign_in_with(email, password)
       #visit new_user_session_path
-      visit "/"
-      click_link "Log in"
+      visit new_user_session_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
       click_button 'Log in'
     end
 
-    def sign_in
+    def create_user_and_sign_in
       user = create(:user)
-      visit "/"
-      click_link "Log in"
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
+      sign_up_with(user.email, user.password)
+      sign_in_with(user.email, user.password)
     end
   end
 end
