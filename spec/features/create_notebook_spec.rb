a5540a0ce0966a33af69852729a0a48eade600ea
Przemek <przemek@krowinski.eu>
2015-01-26 05:57:49 +0100

    Fix links and add sleep for modal animation

--- spec/features/create_notebook_spec.rb
+++ spec/features/create_notebook_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 feature 'create notebook' do
   before do
-    sign_in
+    create_user_and_sign_in
   end
   scenario 'with valid name', :js => true do
     notebook = build(:notebook)
     visit '/'
-    click_link('new-notebook')
+    click_button('new-notebook')
+
+    sleep 2
 
     fill_in 'notebook_name', with: notebook.name
+
+    click_button('Create notebook')
 
     expect(find('#left-menu').find('#notebook-list')).to have_content(notebook.name)
   end
